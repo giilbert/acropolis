6e5fbe2987f3f9091cb7881e4793be6c5bb3c90a
gil <gilbertzhang1208@gmail.com>
2022-08-08 13:50:44 -0400

    better logging

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,30 @@ mod resources;
 mod systems;
 mod utils;
 
+use fern::colors::{Color, ColoredLevelConfig};
 use lib::application::Application;
 
 fn main() {
+    let mut colors = ColoredLevelConfig::new()
+        .info(Color::Blue)
+        .warn(Color::Yellow)
+        .debug(Color::Yellow)
+        .error(Color::Red)
+        .trace(Color::Green);
+
+    fern::Dispatch::new()
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "{} [ {} ] {} - {}",
+                chrono::Local::now().format("%H:%M:%S"),
+                colors.color(record.level()),
+                record.target(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::max())
+        .chain(std::io::stdout())
+        .apply()
+        .unwrap();
     Application::new().run();
 }
